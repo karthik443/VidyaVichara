43032777e47a6734fc8af344e1c1ba3fe07aed66
Karthik Nallala <karthiknallala96@gmail.com>
2025-09-27 00:46:29 +0530

    changes

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import QuestionBoard from "./components/QuestionBoard";
+
+function App() {
+  return (
+    <div>
+      <h1>VidyaVichara</h1>
+      <QuestionBoard />
+    </div>
+  );
+}
+
+export default App;
