3a1c3febc832818fbe5bd3cc7e2ee9ae294af1e3
runtimeerror11 <suparshwapatil28@gmail.com>
2025-09-27 02:49:01 +0530

    Added Upvote feature

--- Backend/models/question.js
+++ Backend/models/question.js
@@ -1,16 +1,25 @@
 import { timeStamp } from "console";
 import mongoose, { mongo } from "mongoose";
-const questoinSchema = new mongoose.Schema({
-    author:{type:String,default:"Anonymus"},
-    text:{type:String , required:true},
-    answer:{type:String,default:"Not Answerd"},
+const questoinSchema = new mongoose.Schema(
+  {
+    author: { type: String, default: "Anonymus" },
+    text: { type: String, required: true },
+    answer: { type: String, default: "Not Answerd" },
     status: {
-    type: String,
-    enum: ["unanswered", "answered", "important"],
-    default: "unanswered",
+      type: String,
+      enum: ["unanswered", "answered", "important"],
+      default: "unanswered",
+    },
+    isDeleted: { type: Boolean, default: false },
+    upvotes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // reference to the user who upvoted
+      },
+    ],
   },
-   isDeleted:{type:Boolean, default:false}
-},{timeStamp:true})
+  { timeStamp: true }
+);
 
-const Question = mongoose.model("question",questoinSchema);
+const Question = mongoose.model("question", questoinSchema);
 export default Question;
