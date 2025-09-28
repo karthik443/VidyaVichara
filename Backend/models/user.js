d47585698b5825e1d8dd0af02061b94667deb1b4
suyashnpande <suyashpande108@gmail.com>
2025-09-28 10:50:28 +0530

    Password

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,7 +1,11 @@
 import mongoose, { mongo } from "mongoose";
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10; 
+
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, default: "Anonymus" },
+    name: { type: String, default: "Anonymous" },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: {
@@ -11,7 +15,47 @@ const userSchema = new mongoose.Schema(
     },
     isBlocked: { type: Boolean, default: false },
   },
-  { timeStamps: true }
+  { timestamps: true } 
 );
-const User = mongoose.model("user", userSchema);
+
+// Pre-save hook to hash password
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // Only hash if password is new
+  try {
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to compare password
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+  return bcrypt.compare(plainPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 export default User;
+
+
+
+// import mongoose, { mongo } from "mongoose";
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, default: "Anonymus" },
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true },
+//     role: {
+//       type: String,
+//       enum: ["student", "teacher"],
+//       default: "student",
+//     },
+//     isBlocked: { type: Boolean, default: false },
+//   },
+//   { timeStamps: true }
+// );
+// const User = mongoose.model("user", userSchema);
+// export default User;
